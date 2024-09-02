02bc2855e3f43cad66b0762247089cf6e02a0da9
Patricio Pilco <pattopilco@gmail.com>
2024-09-01 19:59:36 -0500

    create api

--- README.md
+++ README.md
@@ -2,4 +2,26 @@
 
 * JCommander
 * feign
-* feign-json
+* feign-gson
+
+## Gradle
+* distribution
+* distZip
+
+
+## Bat
+
+``` batch
+cd build
+cd distribution
+unzip job-search
+job-search.bat
+```
+
+## Commands
+
+``` batch
+job-search java --location Mexico
+job-search java --location Mexico --full-time
+```
+
---
+++ src/main/java/com/xpoch/jobsearch/api/APIJobs.java
@@ -0,0 +1,17 @@
+package com.xpoch.jobsearch.api;
+
+
+import com.xpoch.jobsearch.JobPosition;
+import feign.Headers;
+import feign.QueryMap;
+import feign.RequestLine;
+
+import java.util.List;
+import java.util.Map;
+
+@Headers("Accept: application/json")
+public interface APIJobs {
+
+    @RequestLine("GET /positions.json")
+    List<JobPosition> jobs(@QueryMap Map<String, Object> queryMap);
+}
